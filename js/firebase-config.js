794e0763834bc268451a1cb30b78d47b670e3a71
Nilxxx11 <80600547+Nilxxx11@users.noreply.github.com>
2026-01-17 10:15:40 -0500

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,21 @@
+// Inicializar Firebase
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_PROYECTO.firebaseapp.com",
+    databaseURL: "https://TU_PROYECTO.firebaseio.com",
+    projectId: "TU_PROYECTO",
+    storageBucket: "TU_PROYECTO.appspot.com",
+    messagingSenderId: "TU_SENDER_ID",
+    appId: "TU_APP_ID"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
+
+export { database, auth };
